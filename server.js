29f2eac69de35729c7ce0a1dde4d67dbb40e3255
jre23 <jre.estrada@gmail.com>
2021-01-21 00:15:22 -0800

    added comments and process.env variables to server.js, renamed api.js to api-routes.js

--- server.js
+++ server.js
@@ -1,28 +1,33 @@
+// require dependencies
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-
-const PORT = 3000;
-
+// configure the PORT
+const PORT = process.env.PORT || 3000;
+// initialize express app
 const app = express();
-
+// use morgan http request logger middleware
 app.use(logger("dev"));
-
+// use compression middleware
 app.use(compression());
-app.use(express.urlencoded({ extended: true }));
+// use middleware to handle json, string, or array request objects
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
-
+// serve static content for the app from the public directory
 app.use(express.static("public"));
-
-mongoose.connect("mongodb://localhost/budget", {
+// routes
+app.use(require("./routes/api-routes.js"));
+// set up connection to mongodb using mongoose
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
-
-// routes
-app.use(require("./routes/api.js"));
-
+// start the server
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
