5046608d9f4acd4d7ff4fa2062ca7f4145c93f07
jre23 <jre.estrada@gmail.com>
2021-01-20 23:30:40 -0800

    created a db.js for the indexedDB, made a link to it in index.html

---
+++ public/db.js
@@ -0,0 +1,65 @@
+const {
+    post
+} = require("../routes/api");
+
+// create a new request for a budget indexedDB
+const request = window.indexedDB.open("budget", 1);
+// create schema
+request.onupgradeneeded = event => {
+    const db = event.target.result;
+    // create an objectStore called "pending" and set autoIncrement to true
+    db.createObjectStore("pending", {
+        autoIncrement: true
+    });
+};
+// if request is successful, check if the browser is online then check the database
+request.onsuccess = event => {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+// if request returns an error
+request.onerror = event => {
+    console.log("======onerror event======");
+    console.log(event);
+}
+// function to save a record if browser if offline
+const saveRecord = record => {
+    // create a transaction on the pending db with readwrite access
+    const transaction = db.transaction(["pending"], "readwrite");
+    // access the pending objectStore
+    const pendingStore = transaction.objectStore("pending");
+    // add the record to the pending objectStore
+    pendingStore.add(record);
+}
+// function to check the budget indexedDB to see if there is any pending object stores and if so, add them to the mongodb using api/transaction/bulk post method
+const checkDatabase = event => {
+    // create a transaction on the pending db with readwrite access
+    const transaction = db.transaction(["pending"], "readwrite");
+    // access the pending objectStore
+    const pendingStore = transaction.objectStore("pending");
+    // get all records (if any) from the pending objectStore
+    const allPending = pendingStore.getAll();
+    // check if the pendingStore has anything in it, if so post it to mongodb then clear the pendingStore
+    if (allPending.result.length > 0) {
+        fetch("api/transaction/bulk", {
+            method: "POST",
+            body: JSON.stringify(allPending.result),
+            headers: {
+                Accept: "application/json, text/plain, */*",
+                "Content-Type": "application/json",
+            },
+        }).then(res => {
+            res.json();
+        }).then(() => {
+            // create a transaction on the pending db with readwrite access
+            const transaction = db.transaction(["pending"], "readwrite");
+            // access the pending objectStore
+            const pendingStore = transaction.objectStore("pending");
+            // get all records (if any) from the pending objectStore
+            pendingStore.clear();
+        });
+    }
+}
