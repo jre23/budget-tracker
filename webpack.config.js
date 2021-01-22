b9972613f7c1a16176c7350c6edf3716576c8943
jre23 <jre.estrada@gmail.com>
2021-01-22 14:16:31 -0800

    added webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,10 @@
+// require dependencies
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const {
+    webpack
+} = require("webpack");
+
 // configure webpack
 const config = {
     entry: "./public/assets/js/index.js",
@@ -5,7 +12,23 @@ const config = {
         path: __dirname + "/public/dist",
         filename: "bundle.js"
     },
-    mode: "development"
+    mode: "development",
+    plugins: [new WebpackPwaManifest({
+        name: "Budget Tracker App",
+        short_name: "Budget App",
+        description: "PWA Budget Tracker",
+        background_color: "white",
+        theme_color: "blue",
+        start_url: "/",
+        display: "standalone",
+        inject: true,
+        fingerprints: false,
+        icons: [{
+            src: path.resolve("public/assets/images/icons/icon-192x192.png"),
+            sizes: [192, 512],
+            destination: path.join("assets", "icons")
+        }]
+    })]
 };
-
+// export config
 module.exports = config;
