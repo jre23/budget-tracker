c4cf688d8990ceba5c7496da3705f054c0766573
jre23 <jre.estrada@gmail.com>
2021-01-22 20:22:33 -0800

    added comments to transaction.js

--- models/transaction.js
+++ models/transaction.js
@@ -1,25 +1,24 @@
+// require dependencies
 const mongoose = require("mongoose");
-
+// define mongoose Schema constructor
 const Schema = mongoose.Schema;
-
-const transactionSchema = new Schema(
-  {
-    name: {
-      type: String,
-      trim: true,
-      required: "Enter a name for transaction"
-    },
-    value: {
-      type: Number,
-      required: "Enter an amount"
-    },
-    date: {
-      type: Date,
-      default: Date.now
-    }
+// define transaction schema
+const transactionSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: "Enter a name for transaction"
+  },
+  value: {
+    type: Number,
+    required: "Enter an amount"
+  },
+  date: {
+    type: Date,
+    default: Date.now
   }
-);
-
+});
+// define Transaction mongoose model
 const Transaction = mongoose.model("Transaction", transactionSchema);
-
+// export Transaction model
 module.exports = Transaction;
